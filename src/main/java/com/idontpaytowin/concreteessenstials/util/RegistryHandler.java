2a0d5dd9a05364799abb7ca02a0e1fee5f73aa79
Idontpaytowin <74816571+Idontpaytowin@users.noreply.github.com>
2020-12-16 10:52:29 +0100

    Update 0.0.3

--- src/main/java/com/idontpaytowin/concreteessenstials/util/RegistryHandler.java
+++ src/main/java/com/idontpaytowin/concreteessenstials/util/RegistryHandler.java
@@ -4,8 +4,13 @@ import com.idontpaytowin.concreteessenstials.ConcreteEssentials;
 import com.idontpaytowin.concreteessenstials.blocks.BlockItemBase;
 import com.idontpaytowin.concreteessenstials.blocks.MultiColorConcretePowder;
 import com.idontpaytowin.concreteessenstials.items.Materials;
+import com.idontpaytowin.concreteessenstials.tools.ModItemTier;
 import net.minecraft.block.Block;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.PickaxeItem;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,10 +26,24 @@ public class RegistryHandler {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    //Items
-    public static final RegistryObject<Item> MULTICOLOR_DYE = ITEMS.register("multicolor_dye", Materials::new);
-    public static final RegistryObject<Item> MULTICOLOR_DYE_PART1 = ITEMS.register("multicolor_dye_part1", Materials::new);
-    public static final RegistryObject<Item> MULTICOLOR_DYE_PART2 = ITEMS.register("multicolor_dye_part2", Materials::new);
+    // Items
+    public static final RegistryObject<Item> MULTI_COLOR_DYE = ITEMS.register("multi_color_dye", Materials::new);
+    public static final RegistryObject<Item> MULTI_COLOR_DYE_PART1 = ITEMS.register("multi_color_dye_part1", Materials::new);
+    public static final RegistryObject<Item> MULTI_COLOR_DYE_PART2 = ITEMS.register("multi_color_dye_part2", Materials::new);
+    public static final RegistryObject<Item> PILE_OF_COBBLESTONE = ITEMS.register("pile_of_cobblestone", Materials::new);
+    public static final RegistryObject<Item> PILE_OF_GRAVEL = ITEMS.register("pile_of_gravel", Materials::new);
+    public static final RegistryObject<Item> PILE_OF_DIRT = ITEMS.register("pile_of_dirt", Materials::new);
+    public static final RegistryObject<Item> PILE_OF_SAND = ITEMS.register("pile_of_sand", Materials::new);
+    public static final RegistryObject<Item> IRON_HAMMER = ITEMS.register("iron_hammer", Materials::new);
+
+    //public static final RegistryObject<Item> WOODEN_HAMMER = ITEMS.register("pile_of_sand", Materials::new);
+    //public static final RegistryObject<Item> STONE_HAMMER = ITEMS.register("pile_of_sand", Materials::new);
+    //public static final RegistryObject<Item> GOLD_HAMMER = ITEMS.register("pile_of_sand", Materials::new);
+    //public static final RegistryObject<Item> DIAMOND_HAMMER = ITEMS.register("pile_of_sand", Materials::new);
+    //public static final RegistryObject<Item> NETHERITE_HAMMER = ITEMS.register("pile_of_sand", Materials::new);
+
+    // Tools
+
 
     // Blocks
     public static final RegistryObject<Block> MULTI_COLOR_CONCRETE_POWDER = BLOCKS.register("multi_color_concrete_powder", MultiColorConcretePowder::new);
